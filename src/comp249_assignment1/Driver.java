0f3c2d032845ced6b460c56ad878e9fcb77f49b7
Zubeda Wajid Hameed <114757482+ZubedaWajid@users.noreply.github.com>
2023-02-06 21:27:14 -0500

    Update Driver.java

--- src/comp249_assignment1/Driver.java
+++ src/comp249_assignment1/Driver.java
@@ -3,6 +3,10 @@ package comp249_assignment1;
 //Assignment 1
 //Written by: Aniss Chalah 40251256, Zubeda Wajid Hameed 40246990
 //-----------------------------------------------------
+
+
+// The point of this program is to create a snakes and ladder game which consists of two players that take turns to roll the dice and have the objective of reaching the 100th position to win the game.
+// This game uses the regular rules of a snakes and ladder game. Currently the game is set to two players but it can be modified to hold more players.
 import java.io.IOException;
 import java.util.Scanner;
 
